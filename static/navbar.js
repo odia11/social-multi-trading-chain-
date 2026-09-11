b14d57fedf2ebfd6d40879270b64a24491bb9f92
Degentrader1990 <55651247+odia11@users.noreply.github.com>
2026-09-11 22:53:23 +0200

    Bust mobile Home performance cache

--- static/navbar.js
+++ static/navbar.js
@@ -30,7 +30,7 @@
   var here=location.pathname.replace(/\/+$/,'')||'/';
   if(here!=='/' || !window.matchMedia('(max-width:767px)').matches) return;
   var css=document.createElement('link'); css.rel='stylesheet'; css.href='/static/home-mobile.css?v=3'; document.head.appendChild(css);
-  var js=document.createElement('script'); js.src='/static/home-mobile.js?v=2'; js.defer=true; document.head.appendChild(js);
+  var js=document.createElement('script'); js.src='/static/home-mobile.js?v=3'; js.defer=true; document.head.appendChild(js);
 })();
 
 var _NB_LIVE_CHAINS = ['solana', 'bsc', 'base', 'arbitrum', 'polygon', 'robinhood'];
