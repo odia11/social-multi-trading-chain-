67bf80704f38032a6f16406145aaa776e74346a6
Degentrader1990 <55651247+odia11@users.noreply.github.com>
2026-09-13 19:24:55 +0200

    Bump navbar connect asset version

--- mobile_ui_hotfix.py
+++ mobile_ui_hotfix.py
@@ -19,7 +19,7 @@ def install(dashboard_module):
         + marker + '></script>'
         '<script src="/static/mobile-nav-post-force.js?v=2" defer '
         + marker + '></script>'
-        '<script src="/static/navbar-connect-state.js?v=3" defer '
+        '<script src="/static/navbar-connect-state.js?v=4" defer '
         + marker + '></script>'
     )
 
