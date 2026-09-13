d95d4d5065bc29b3ec1aea8211e9251db89e9896
Degentrader1990 <55651247+odia11@users.noreply.github.com>
2026-09-13 20:14:28 +0200

    Bump global connect control asset version

--- mobile_ui_hotfix.py
+++ mobile_ui_hotfix.py
@@ -21,7 +21,7 @@ def install(dashboard_module):
         + marker + '></script>'
         '<script src="/static/navbar-connect-state.js?v=5" defer '
         + marker + '></script>'
-        '<script src="/static/mobile-connect-button.js?v=4" defer '
+        '<script src="/static/mobile-connect-button.js?v=5" defer '
         + marker + '></script>'
         '<script src="/static/guest-menu-auth-fix.js?v=1" defer '
         + marker + '></script>'
