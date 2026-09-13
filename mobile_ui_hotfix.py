cb8d730f8d9b2f19bf4ee1e5e0e421a9c0a7537a
Degentrader1990 <55651247+odia11@users.noreply.github.com>
2026-09-13 19:05:09 +0200

    Load navbar connect state controller

--- mobile_ui_hotfix.py
+++ mobile_ui_hotfix.py
@@ -19,6 +19,8 @@ def install(dashboard_module):
         + marker + '></script>'
         '<script src="/static/mobile-nav-post-force.js?v=2" defer '
         + marker + '></script>'
+        '<script src="/static/navbar-connect-state.js?v=1" defer '
+        + marker + '></script>'
     )
 
     @app.after_request
