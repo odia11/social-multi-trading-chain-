c055e76aa40469952fc4b6790ac57b6101e5010f
Degentrader1990 <55651247+odia11@users.noreply.github.com>
2026-09-13 19:50:28 +0200

    Bust mobile Connect auth-state cache

--- mobile_ui_hotfix.py
+++ mobile_ui_hotfix.py
@@ -21,7 +21,7 @@ def install(dashboard_module):
         + marker + '></script>'
         '<script src="/static/navbar-connect-state.js?v=4" defer '
         + marker + '></script>'
-        '<script src="/static/mobile-connect-button.js?v=1" defer '
+        '<script src="/static/mobile-connect-button.js?v=2" defer '
         + marker + '></script>'
     )
 
